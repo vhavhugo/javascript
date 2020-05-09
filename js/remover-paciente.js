360cd1d97f65bcfa465fb0c948754d3fce893742
vhavhugo <vhavhugo@hotmail.com>
2020-05-09 01:21:41 -0300

    07 - delengo eventos

--- js/remover-paciente.js
+++ js/remover-paciente.js
@@ -1,7 +1,9 @@
 var pacientes = document.querySelectorAll(".paciente");
 
-pacientes.forEach(function(paciente){
-    paciente.addEventListener("dblclick", function(){
-        console.log("Fui clicado com um duplo cliquel;");
-    });
+var tabela = document.querySelector("table");
+
+tabela.addEventListener("dblclick", function(event){
+    // var alvoEvento = event.target;
+    // var paiDoAlvo = alvoEvento.parentNode; // tr
+    event.target.parentNode.remove();
 });
