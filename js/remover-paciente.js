424b7e1ce5cdb21a39b192f5599bb3eab9f85dbb
vhavhugo <vhavhugo@hotmail.com>
2020-05-09 01:57:49 -0300

    07_06: Animando a remoção do paciente

--- js/remover-paciente.js
+++ js/remover-paciente.js
@@ -5,5 +5,8 @@ var tabela = document.querySelector("table");
 tabela.addEventListener("dblclick", function(event){
     // var alvoEvento = event.target;
     // var paiDoAlvo = alvoEvento.parentNode; // tr
-    event.target.parentNode.remove();
+    event.target.parentNode.classList.add(".fadeOut");
+    setTimeout(function(){
+        event.target.parentNode.remove();
+    },500);
 });
