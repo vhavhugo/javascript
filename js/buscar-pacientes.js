36d383f7cc69d049445350b9a0b3c09442c430d5
vhavhugo <vhavhugo@hotmail.com>
2020-05-17 19:02:21 -0300

    fim

--- js/buscar-pacientes.js
+++ js/buscar-pacientes.js
@@ -6,13 +6,22 @@ botaoAdicionar.addEventListener("click", function(){
     xhr.open("GET", "//api-pacientes.herokuapp.com/pacientes");
 
     xhr.addEventListener("load", function () {
-        var resposta = xhr.responseText;
-        console.log(resposta);
-        //transforma a string em array
-        var pacientes = JSON.parse(resposta);
-        pacientes.forEach( function (paciente) {
+        var erroAjax = document.querySelector("#erro-ajax");
+
+        if (xhr.status == 200){
+            erroAjax.classList.add("invisivel");
+            var resposta = xhr.responseText;
+            console.log(resposta);
+            //transforma a string em array
+            var pacientes = JSON.parse(resposta);
+            pacientes.forEach(function (paciente) {
             adicionarPacienteNaTabela(paciente);
         });
+    }else{
+        console.log(xhr.status);
+        console.log(xhr.responseText);
+        erroAjax.classList.remove("invisivel");
+    }
         // console.log(typeof  resposta);
 
        // console.log(xhr.responseText);
